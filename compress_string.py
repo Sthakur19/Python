5173ec051377e77f2aa5f383849b44fd4f6fbe7a
Sthakur19 <STHAKUR@ibs-b.hu>
2023-04-17 16:55:21 +0200

    Update

--- compress_string.py
+++ compress_string.py
@@ -13,3 +13,35 @@ def string_compression(input_str):
     return comp_str
     
 print(string_compression(input_str))
+
+# num = int(input("Please enter the number :"))
+
+# def fizzbuzz(n):
+
+#     for i in range(1, n):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("Fizz buzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         elif i % 5 == 0:
+#             print(" buzz")
+#         else:
+#             print(i)
+
+# fizzbuzz(16)
+
+# def fizzbuzz(n):
+#     dict = {
+#         3 : "Fizz",
+#         5 : "Buzz"
+#     }
+#     for i in range(1, n):
+#         result = ""
+#         for key, value in dict.items():
+#             if i % key == 0:
+#                 result += value
+#         if result == "":
+#             result = i
+#         print(result)
+
+# fizzbuzz(16)
