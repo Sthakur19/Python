c32c0003328555a6f7fad1a7a053bd30b4d8bd93
Sthakur19 <STHAKUR@ibs-b.hu>
2023-04-17 11:50:59 +0200

    Fibonacci_series.py using recursion

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -11,18 +11,30 @@
 
 # fibonacci_series(10)
 
-def fibonacci_series2(n):
-    a, b= 0, 1
-    print(a)
-    if n == 1:
-        print(a)
-    else:
-        print(a)
-        print(b)
-    for i in range(2, n):
-        c = a + b
-        a = b
-        b = c 
-        print(b)
+# def fibonacci_series2(n):
+#     a, b= 0, 1
+#     print(a)
+#     if n == 1:
+#         print(a)
+#     else:
+#         print(a)
+#         print(b)
+#     for i in range(2, n):
+#         c = a + b
+#         a = b
+#         b = c 
+#         print(b)
 
-fibonacci_series2(7)
+# fibonacci_series2(7)
+
+
+def fibo(i):
+    if i == 0:
+        return 0
+    if i == 1:
+        return 1
+    return fibo(i-1) + fibo(i-2)
+
+n = 7
+for i in range(n+1):
+    print(fibo(i))
